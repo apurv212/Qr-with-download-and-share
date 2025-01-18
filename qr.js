0d743c53da250c9620c9fcbf01daea09db165fae
apurv212 <harshitsingh0612@gmail.com>
2025-01-18 12:28:04 +0530

    commit

--- qr.js
+++ qr.js
@@ -5,44 +5,44 @@ let jlink = document.getElementById("downloadLink");
 let jshareBtn = document.getElementById("sharebtn");
 
 async function getdata(links) {
-    const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${links}`);
-    const blobs = await response.blob();
-    return URL.createObjectURL(blobs);
+  const response = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${links}`
+  );
+  const blobs = await response.blob();
+  return URL.createObjectURL(blobs);
 }
 
 jbtn.addEventListener("click", generateQRCode);
 
 jtext.addEventListener("keydown", async (event) => {
-     if (event.key === "Enter") 
-        { generateQRCode(); }
-     });
+  if (event.key === "Enter") {
+    generateQRCode();
+  }
+});
 
-async function generateQRCode() { 
-    let store = jtext.value; 
-    let final = await getdata(store); 
-    jimg.src = final; 
-    jlink.href = final; 
-    jlink.style.display = "block";
+async function generateQRCode() {
+  let store = jtext.value;
+  let final = await getdata(store);
+  jimg.src = final;
+  jlink.href = final;
+  jlink.style.display = "block";
 }
 
-jshareBtn.addEventListener("click",async()=>{
-    try {
-        const response = await fetch(jimg.src);
-        const datashare=  await response.blob();
-        const sharefile = new File([datashare],'qrcode.png',{type: "image/png"});
+jshareBtn.addEventListener("click", async () => {
+  try {
+    const response = await fetch(jimg.src);
+    const datashare = await response.blob();
+    const sharefile = new File([datashare], "qrcode.png", {
+      type: "image/png",
+    });
 
-        await navigator.share({
-            title: "apurv shashvat QR Code ",
-            text: "scan this qr code",
-            files: [sharefile]
-        });
-      
-      } catch (err) {
-        console.error('Error sharing QR code:', err);
-      alert("problem in sharing ur qr code")
-      }
-
-    
-})
-
-
+    await navigator.share({
+      title: "apurv shashvat QR Code ",
+      text: "scan this qr code:${jtext.value} ",
+      files: [sharefile],
+    });
+  } catch (err) {
+    console.error("Error sharing QR code:", err);
+    alert("problem in sharing ur qr code");
+  }
+});
